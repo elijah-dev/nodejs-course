8a38c4528d6c307ebf660b944d4c4ec99c348c6f
Ilia_Khrebtov <Ilia_Khrebtov@epam.com>
2023-02-02 12:53:24 +0500

    small fix

--- task-2/src/controllers/user/get-all-users.ts
+++ task-2/src/controllers/user/get-all-users.ts
@@ -2,5 +2,11 @@ import { userModel } from "@model";
 import type { RequestHandler } from "express";
 
 export const getAllUsers: RequestHandler = (_, res) => {
-  res.json(userModel.users);
+  try {
+
+    res.json(userModel.users);
+  } catch (error) {
+    res.status(500);
+    res.send("Smoe thing went wrong")
+  }
 };
